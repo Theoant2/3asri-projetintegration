ef11bdb3b161d9100fe1db6e0eb4c7238a0b02dd
Theoant2 <theoant24@gmail.com>
2022-10-14 17:21:58 +0200

    seance 4

--- scripts/test_gazebo.py
+++ scripts/test_gazebo.py
@@ -21,6 +21,11 @@ scene = moveit_commander.PlanningSceneInterface()
 group_name = "hc10_arm"
 move_group = moveit_commander.MoveGroupCommander(group_name)
 move_group.set_goal_tolerance(0.1)
+move_group.set_goal_joint_tolerance(0.1)
+
+RIGHT_LOOKING_LEFT = Pose(position = Point(x = 0.71, y = -0.24, z = 0.17), orientation = Quaternion(x = -0.534, y = 0.503, z = -0.433, w = 0.524))
+LEFT_LOOKING_RIGHT = Pose(position = Point(x = 0.71, y = 0.24, z = 0.17), orientation = Quaternion(x = -0.534, y = 0.503, z =  0.433, w = -0.524))
+TOP_LOOKING_DOWN = Pose(position = Point(x = 0.65, y = 0.17, z = 1.0), orientation = Quaternion(x = 1.0, y = 0.0, z = 0.0, w = 0.0))
 
 rospy.sleep(1)
 
@@ -37,9 +42,11 @@ print("")
 
 # Test sequence
 print("=== Test sequence ===")
-print("  < trajectory without box >")
-commons.test_movement(move_group, pose = commons.END, pose_name = "end")
-commons.test_movement(move_group, pose = commons.HOME, pose_name = "home")
+print("  < scanning box >")
+commons.test_movement(move_group, pose = TOP_LOOKING_DOWN, pose_name = "top looking down")
+commons.test_movement(move_group, pose = RIGHT_LOOKING_LEFT, pose_name = "right looking left")
+commons.test_movement(move_group, pose = LEFT_LOOKING_RIGHT, pose_name = "left looking right")
+
 
 print("...")
 rospy.sleep(3)
--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ echo "Renaming package"
 mv 3asri-projetintegration hc10_moveit_config
 
 echo "Moving xarco files"
-mv motoman_hc10_support ../motoman
+mv hc10_moveit_config/motoman_hc10_support ../motoman
 
 echo "Setup done"
